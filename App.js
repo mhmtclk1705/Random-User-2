353cd166b8aa298e41231abf599d2155f3e5296b
mhmtclk1705 <mehmetcelik1705@gmail.com>
2022-04-10 18:09:59 +0300

    edit

--- App.js
+++ App.js
@@ -145,7 +145,7 @@ function App() {
           </div>
           <div className="footer">
             <p>
-              <a href="https://www.github.com/mhmtclk1705/" target="_blank" rel="noreferrer">
+              <a href="https://www.github.com/mhmtclk1705/" target="_blank" rel="noopener noreferrer">
                 <span>{"<mhmtclk/>"}</span>
               </a>
               <img src={designSvg} />
